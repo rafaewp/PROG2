a0711eaa1c3a9b4273d710faf6761dcd7e6ce1ab
Rafael Pereira <149113377+rafaewp@users.noreply.github.com>
2026-04-04 07:05:47 -0300

    Refactor EX8.c to improve code formatting

--- EX8.c
+++ EX8.c
@@ -3,8 +3,6 @@
 /* EX 8 --- Considerando a existência de notas (cédulas) nos valores R$ 100, R$ 50, R$ 20, R$ 10, R$ 5, R$ 2 e R$ 1, escreva um programa que capture um valor inteiro em reais (R$) e determine o menor número de notas para se obter o montante fornecido. A função deve ter o seguinte protótipo:
 int total_de_notas(int valor);. */
 
-#include <stdio.h>
-
 int total_de_notas(int valor){
 
     // Variável para armazenar a quantidade calculada em cada volta
@@ -15,7 +13,6 @@ int total_de_notas(int valor){
 
     // Array com as quantidades
     int quantidades[7] = {0, 0, 0, 0, 0, 0, 0};
-
 
     // Percorrendo o Array
     for (int i = 0; i < 7; i++){
